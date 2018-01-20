6b4981a797f5a46dbc31d9f219a7be651b238450
hackboxlive <singhsarvshakti127@gmail.com>
2018-01-21 03:48:12 +0530

    Corrected already correct problem C

--- Codeforces/458/3.cpp
+++ Codeforces/458/3.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int ch[1010];
+long long ch[2010];
 long long modd=1e9+7;
-vector<int> bits;
-vector<int> p;
-long long dp[1010][1010];
-long long inv[1010];
-int req_bits(int n)	{
+vector<long long> bits;
+long long dp[2010][2010];
+long long inv[2010];
+long long req_bits(long long n)	{
 	if(ch[n]!=-1)	{
 		return ch[n];
 	}
-	int v=n;
-	int ans=0;
+	long long v=n;
+	long long ans=0;
 	while(v!=0)	{
 		if(v%2==1)	{
 			ans++;
@@ -36,27 +35,27 @@ long long pow_fast(long long n,long long v)	{
 	return ans;
 }
 void choose()	{
-	for(int i=1;i<=1000;i++)	{
+	for(long long i=1;i<=1000;i++)	{
 		dp[i][1]=i;
 	}
 	inv[1]=1;
-	for(int i=2;i<=1000;i++)	{
+	for(long long i=2;i<=1000;i++)	{
 		inv[i]=pow_fast(i,modd-2);
 	}
-	for(int i=2;i<=1000;i++)	{
-		for(int j=2;j<=i;j++)	{
+	for(long long i=2;i<=1000;i++)	{
+		for(long long j=2;j<=i;j++)	{
 			dp[i][j]=(dp[i][j-1]*(i-j+1))%modd;
 			dp[i][j]=(dp[i][j]*inv[j])%modd;
 		}
 	}
 }
-long long choose_again(int n,int r)	{
+long long choose_again(long long n,long long r)	{
 	if(r>n)	{
 		return 0;
 	}
 	return dp[n][r];
 }
-string to_binary(int n)	{
+string to_binary(long long n)	{
 	string ans="";
 	while(n!=0)	{
 		ans+=to_string(n%2);
@@ -69,8 +68,7 @@ int main()	{
 	choose();
 	memset(ch,-1,sizeof(ch));
 	ch[1]=0;
-	int mm=-1;
-	for(int i=2;i<=1000;i++)	{
+	for(long long i=2;i<=1000;i++)	{
 		ch[i]=req_bits(i);
 	}
 	string s;
@@ -93,52 +91,47 @@ int main()	{
 		cout<<s.length()-1<<endl;
 		return 0;
 	}
-// 	if(s.length()<=9)	{
-// 		int ppp=0;
-// 		for(int i=0;i<s.length();i++)	{
-// 			ppp*=2;
-// 			ppp+=(s[i]-'0');
-// 		}
-// 		int anss=0;
-// 		for(int i=1;i<=ppp;i++)	{
-// 			if(ch[i]==k)	{
-// //				cout<<to_binary(i)<<endl;
-// 				anss++;
-// 			}
-// 		}
-// 		cout<<anss<<endl;
-// 		return 0;
-// 	}
+	if(s.length()<=9)	{
+		long long ppp=0;
+		for(long long i=0;i<s.length();i++)	{
+			ppp*=2;
+			ppp+=(s[i]-'0');
+		}
+		long long anss=0;
+		for(long long i=1;i<=ppp;i++)	{
+			if(ch[i]==k)	{
+//				cout<<to_binary(i)<<endl;
+				anss++;
+			}
+		}
+		cout<<anss<<endl;
+		return 0;
+	}
 	reverse(s.begin(),s.end());
-	for(int i=0;i<s.length();i++)	{
+	for(long long i=0;i<s.length();i++)	{
 		if(s[i]=='1')	{
 			bits.push_back(i+1);
 		}
 	}
 	reverse(bits.begin(),bits.end());
-	// for(int i=0;i<bits.size();i++)	{
-	// 	cout<<bits[i]<<" ";
-	// }
 	reverse(s.begin(),s.end());
 	long long ans=0;
 	//finding answer for k=2,3,4,5
-	int checker=k-1;
-	for(int i=1;i<=1000;i++)	{
-		if(ch[i]==checker)	{
-//			cout<<"checking "<<i<<endl;
+	for(long long i=1;i<=1000;i++)	{
+		if(ch[i]==k-1)	{
 			if(bits.size()>=i)	{
-//				cout<<i<<endl;
 				ans++;
 				ans%=modd;
 			}
-			for(int j=i;j>=1;j--)	{
+			for(long long j=i;j>=1;j--)	{
+			    if(bits.size()<=i-j)    {
+			        break;
+			    }
 				if(bits[i-j]-1<j)	{
 					break;
 				}
-//				cout<<choose_again(bits[i-j]-1,j)<<endl;
 				ans+=choose_again(bits[i-j]-1,j);
 				ans%=modd;
-//				cout<<ans<<endl;
 			}
 		}
 	}
